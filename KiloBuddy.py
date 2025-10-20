3ae7d97d5fd0b0ddd14d93c3f63c4677b7680f3e
Michael Creel <michaelcreel815@gmail.com>
2025-10-19 23:20:22 -0600

    Created Gemini Variables

--- KiloBuddy.py
+++ KiloBuddy.py
@@ -3,3 +3,6 @@ import re
 import os
 import google.generativeai as genai
 
+API_TIMEOUT = 10 # Duration for API Response in seconds
+GEMINI_API_KEY = "" # API Key for calling Gemini API, loaded from gemini_api_key file
+PROMPT = "Return 'Prompt not loaded'." # Prompt for Gemini API Key call, loaded from prompt file
